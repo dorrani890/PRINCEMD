4472a32fdb40b3a165e05b43608da3a5b277d65c
GDS <112704679+DASTAGHIR@users.noreply.github.com>
2025-01-12 19:02:28 +0500

    Update

--- update.sh
+++ update.sh
@@ -1,30 +1,21 @@
-#!/data/data/com.termux/files/usr/bin/bash  
-BOT_DIR="PRINCEMD"  
-BOT_REPO="https://github.com/DASTAGHIR/$BOT_DIR" 
+#!/bin/bash
+BOT_DIR="PRINCEMD"
+BOT_REPO="https://github.com/DASTAGHIR/$BOT_DIR"
 
-GREEN='\033[32m' 
-BOLD='\033[1m' 
-RESET='\033[0m' 
-  
-if [[ $(basename "$PWD") == "$BOT_DIR" ]]; then 
-    echo -e "${BOLD}${GREEN}Cloning repository \"$BOT_REPO\" into \"$HOME\"...\n${RESET}"
-    rm -rf "$HOME/$BOT_DIR" && git clone "$BOT_REPO" && cd "$HOME/$BOT_DIR" && yarn --ignore-scripts && npm install --legacy-peer-deps && cd
+GREEN='\033[32m'
+BOLD='\033[1m'
+RESET='\033[0m'
+
+# Ensure you're in the correct directory
+cd /app
+
+if [ -d "$BOT_DIR" ]; then
+    echo -e "${BOLD}${GREEN}Updating dependencies for $BOT_DIR...\n${RESET}"
+    cd "$BOT_DIR"
+    git pull origin main
+    npm install --legacy-peer-deps
     echo -e "${BOLD}${GREEN}Starting $BOT_DIR...\n${RESET}"
-    cd "$BOT_DIR" && npm start
+    npm start
 else
-    echo -e "${BOLD}${GREEN}Current location: \"$HOME\"\n${RESET}"
-    cd "$HOME"
-    if [ -e "$HOME/$BOT_DIR" ]; then
-        echo -e "${BOLD}${GREEN}Heading to \"$BOT_DIR\".\n${RESET}"
-        cd "$HOME/$BOT_DIR"
-        echo -e "${BOLD}${GREEN}Cloning repository \"$BOT_REPO\" in \"$HOME\"...\n${RESET}"
-        cd && rm -rf "$BOT_DIR" && git clone "$BOT_REPO" && cd "$BOT_DIR" && yarn --ignore-scripts && npm install --legacy-peer-deps && cd
-        echo -e "${BOLD}${GREEN}Starting $BOT_DIR...\n${RESET}"
-        cd "$BOT_DIR" && npm start
-    else
-        echo -e "${BOLD}${GREEN}\"$BOT_DIR\" does not exist, cloning \"$BOT_REPO\" in \"$HOME\"...\n${RESET}"
-        git clone "$BOT_REPO" && cd "$BOT_DIR" && yarn --ignore-scripts && npm install --legacy-peer-deps && cd
-        echo -e "${BOLD}${GREEN}Starting $BOT_DIR...\n${RESET}"
-        cd "$BOT_DIR" && npm start
-    fi
+    echo -e "${BOLD}${GREEN}$BOT_DIR directory not found, please ensure the bot is deployed correctly.\n${RESET}"
 fi
