1c1cfcb2a2c3bb1e483766be22596e71032e95f8
GDS <112704679+DASTAGHIR@users.noreply.github.com>
2025-01-12 18:49:12 +0500

    Update

--- update.sh
+++ update.sh
@@ -10,7 +10,7 @@ RESET='\033[0m'
 if [[ $(basename "$PWD") == "$BOT_DIR" ]]; then 
     if [ -e "$DB_FILE" ]; then 
         echo -e "${BOLD}${GREEN}Moving \"$DB_FILE\" to \"$HOME\" and cloning repository \"$BOT_REPO\" into \"$HOME\"...\n${RESET}"
-        mv "$HOME/$BOT_DIR/$DB_FILE" "$HOME" && cd && rm -rf "$HOME/$BOT_DIR" && git clone "$BOT_REPO" && cd "$HOME/$BOT_DIR" && yarn --ignore-scripts && npm install && cd
+        mv "$HOME/$BOT_DIR/$DB_FILE" "$HOME" && cd && rm -rf "$HOME/$BOT_DIR" && git clone "$BOT_REPO" && cd "$HOME/$BOT_DIR" && yarn --ignore-scripts && npm install --legacy-peer-deps && cd
         if [ -e "$HOME/$DB_FILE" ]; then
             echo -e "${BOLD}${GREEN}Rescuing file \"$DB_FILE\" and moving it to \"$BOT_DIR\".\n${RESET}"
             echo -e "${BOLD}${GREEN}Starting $BOT_DIR...\n${RESET}"
@@ -22,7 +22,7 @@ if [[ $(basename "$PWD") == "$BOT_DIR" ]]; then
         fi
     else
         echo -e "${BOLD}${GREEN}\"$DB_FILE\" not found in \"$BOT_DIR\", cloning repository \"$BOT_REPO\" to \"$HOME\"...\n${RESET}"
-        cd && rm -rf "$HOME/$BOT_DIR" && git clone "$BOT_REPO" && cd "$HOME/$BOT_DIR" && yarn --ignore-scripts && npm install && cd
+        cd && rm -rf "$HOME/$BOT_DIR" && git clone "$BOT_REPO" && cd "$HOME/$BOT_DIR" && yarn --ignore-scripts && npm install --legacy-peer-deps && cd
         if [ -e "$HOME/$DB_FILE" ]; then
             echo -e "${BOLD}${GREEN}Rescuing file \"$DB_FILE\" and moving it to \"$BOT_DIR\".\n${RESET}"
             echo -e "${BOLD}${GREEN}Starting $BOT_DIR...\n${RESET}"
@@ -41,7 +41,7 @@ else
         cd "$HOME/$BOT_DIR"
         if [ -e "$HOME/$BOT_DIR/$DB_FILE" ]; then
             echo -e "${BOLD}${GREEN}Moving \"$DB_FILE\" to \"$HOME\" and cloning repository \"$BOT_REPO\" in \"$HOME\"...\n${RESET}"
-            mv "$HOME/$BOT_DIR/$DB_FILE" "$HOME" && cd && rm -rf "$BOT_DIR" && git clone "$BOT_REPO" && cd "$BOT_DIR" && yarn --ignore-scripts && npm install && cd
+            mv "$HOME/$BOT_DIR/$DB_FILE" "$HOME" && cd && rm -rf "$BOT_DIR" && git clone "$BOT_REPO" && cd "$BOT_DIR" && yarn --ignore-scripts && npm install --legacy-peer-deps && cd
             if [ -e "$HOME/$DB_FILE" ]; then
                 echo -e "${BOLD}${GREEN}Rescuing file \"$DB_FILE\" and moving it to \"$BOT_DIR\".\n${RESET}"
                 mv "$HOME/$DB_FILE" "$HOME/$BOT_DIR/" && cd "$BOT_DIR" &&
@@ -55,13 +55,13 @@ else
             fi
         else
             echo -e "${BOLD}${GREEN}\"$DB_FILE\" does not exist, cloning \"$BOT_REPO\" in \"$HOME\"...\n${RESET}"
-            cd && rm -rf "$BOT_DIR" && git clone "$BOT_REPO" && cd "$BOT_DIR" && yarn --ignore-scripts && npm install &&
+            cd && rm -rf "$BOT_DIR" && git clone "$BOT_REPO" && cd "$BOT_DIR" && yarn --ignore-scripts && npm install --legacy-peer-deps &&
             echo -e "${BOLD}${GREEN}Starting $BOT_DIR...\n${RESET}"
             npm start
         fi
     else
         echo -e "${BOLD}${GREEN}\"$BOT_DIR\" does not exist, cloning \"$BOT_REPO\" in \"$HOME\"...\n${RESET}"
-        git clone "$BOT_REPO" && cd "$BOT_DIR" && yarn --ignore-scripts && npm install && cd
+        git clone "$BOT_REPO" && cd "$BOT_DIR" && yarn --ignore-scripts && npm install --legacy-peer-deps && cd
         if [ -e "$HOME/$DB_FILE" ]; then
             echo -e "${BOLD}${GREEN}I have found a file \"$DB_FILE\" in \"$HOME\", moving it to \"$BOT_DIR\".\n${RESET}"
             mv "$HOME/$DB_FILE" "$HOME/$BOT_DIR/" && cd "$BOT_DIR" && 
