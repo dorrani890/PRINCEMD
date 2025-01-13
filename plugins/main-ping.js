f80c1cde02ab21a8a398f892d3772749460f81a4
GDS <112704679+DASTAGHIR@users.noreply.github.com>
2025-01-13 08:17:01 +0500

    Update main-ping.js

--- plugins/main-ping.js
+++ plugins/main-ping.js
@@ -7,7 +7,7 @@ let handler = async (m, { conn }) => {
          exec(`neofetch --stdout`, (error, stdout, stderr) => {
           let child = stdout.toString("utf-8");
           let ssd = child.replace(/Memory:/, "Ram:");
-          m.reply(`⚫⚪*${mssg.ping}* : ${latensi.toFixed(4)} *Milliseconds (ms)*`);
+          m.reply(`⚪ *${mssg.ping}* : ${latensi.toFixed(4)} *Milliseconds (ms)*`);
             });
 }
 handler.help = ['ping']
